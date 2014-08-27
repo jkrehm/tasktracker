6293132d42d3df2b22c4d6bc8f6665cdbeedf120
Jonathan Rehm <jkrehm@gmail.com>
2014-08-26 23:29:32 -0700

    Add ability to remove multiple tasks at once

--- public/js/app/models/task.js
+++ public/js/app/models/task.js
@@ -6,6 +6,7 @@ define(['backbone'], function (Backbone) {
         defaults: {
             descr         : '',
             running       : false,
+            selected      : false,
             time          : 0,
             timerInterval : 0
         },
